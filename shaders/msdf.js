2623466d71b5d7e6322d015584fd1d1f3de6f737
Hugofgx9 <hugo.faugeroux@mmibordeaux.com>
2020-12-02 21:59:24 +0100

    msdf shader from GLSL300 to GLSL100

--- shaders/msdf.js
+++ shaders/msdf.js
@@ -22,40 +22,77 @@ module.exports = function createMSDFShader (opt) {
       map: { type: 't', value: map || new THREE.Texture() },
       color: { type: 'c', value: new THREE.Color(color) }
     },
-    vertexShader: [
-      'attribute vec2 uv;',
-      'attribute vec4 position;',
-      'uniform mat4 projectionMatrix;',
-      'uniform mat4 modelViewMatrix;',
-      'varying vec2 vUv;',
-      'void main() {',
-      'vUv = uv;',
-      'gl_Position = projectionMatrix * modelViewMatrix * position;',
-      '}'
-    ].join('\n'),
-    fragmentShader: [
-      '#ifdef GL_OES_standard_derivatives',
-      '#extension GL_OES_standard_derivatives : enable',
-      '#endif',
-      'precision ' + precision + ' float;',
-      'uniform float opacity;',
-      'uniform vec3 color;',
-      'uniform sampler2D map;',
-      'varying vec2 vUv;',
 
-      'float median(float r, float g, float b) {',
-      '  return max(min(r, g), min(max(r, g), b));',
-      '}',
+    //version GLSL 100
+    vertexShader: '#version 300 es\n' + [ 
+      'in vec2 uv;', 
+      'in vec4 position;', 
+      'uniform mat4 projectionMatrix;', 
+      'uniform mat4 modelViewMatrix;', 
+      'out vec2 vUv;', 
 
-      'void main() {',
-      '  vec3 sample = ' + (negate ? '1.0 - ' : '') + 'texture2D(map, vUv).rgb;',
-      '  float sigDist = median(sample.r, sample.g, sample.b) - 0.5;',
-      '  float alpha = clamp(sigDist/fwidth(sigDist) + 0.5, 0.0, 1.0);',
-      '  gl_FragColor = vec4(color.xyz, alpha * opacity);',
-      alphaTest === 0
-        ? ''
-        : '  if (gl_FragColor.a < ' + alphaTest + ') discard;',
-      '}'
-    ].join('\n')
+      'void main() {', 
+      'vUv = uv;', 
+      'gl_Position = projectionMatrix * modelViewMatrix * position;', 
+      '}' ].join('\n'), 
+    fragmentShader: [ 
+    '#version 300 es', 
+    '#ifdef GL_OES_standard_derivatives', 
+    '#extension GL_OES_standard_derivatives : enable', 
+    '#endif', 
+    'precision ' + precision + ' float;', 
+    'uniform float opacity;', 
+    'uniform vec3 color;', 
+    'uniform sampler2D map;', 
+    'in vec2 vUv;', 
+    'out vec4 myOutputColor;', 
+    'float median(float r, float g, float b) {', 
+    ' return max(min(r, g), min(max(r, g), b));', 
+    '}', 
+
+    'void main() {', 
+    ' vec3 s = ' + (negate ? '1.0 - ' : '') + 'texture(map, vUv).rgb;', 
+    ' float sigDist = median(s.r, s.g, s.b) - 0.5;', 
+    ' float alpha = clamp(sigDist/fwidth(sigDist) + 0.5, 0.0, 1.0);', 
+    ' myOutputColor = vec4(color.xyz, alpha * opacity);', 
+    alphaTest === 0 ? '' : ' if (myOutputColor.a < ' + alphaTest + ') discard;', 
+    '}' ].join('\n')
+
+    // //version GLSL 300
+    // vertexShader: [
+    //   'attribute vec2 uv;',
+    //   'attribute vec4 position;',
+    //   'uniform mat4 projectionMatrix;',
+    //   'uniform mat4 modelViewMatrix;',
+    //   'varying vec2 vUv;',
+    //   'void main() {',
+    //   'vUv = uv;',
+    //   'gl_Position = projectionMatrix * modelViewMatrix * position;',
+    //   '}'
+    // ].join('\n'),
+    // fragmentShader: [
+    //   '#ifdef GL_OES_standard_derivatives',
+    //   '#extension GL_OES_standard_derivatives : enable',
+    //   '#endif',
+    //   'precision ' + precision + ' float;',
+    //   'uniform float opacity;',
+    //   'uniform vec3 color;',
+    //   'uniform sampler2D map;',
+    //   'varying vec2 vUv;',
+
+    //   'float median(float r, float g, float b) {',
+    //   '  return max(min(r, g), min(max(r, g), b));',
+    //   '}',
+
+    //   'void main() {',
+    //   '  vec3 sample = ' + (negate ? '1.0 - ' : '') + 'texture2D(map, vUv).rgb;',
+    //   '  float sigDist = median(sample.r, sample.g, sample.b) - 0.5;',
+    //   '  float alpha = clamp(sigDist/fwidth(sigDist) + 0.5, 0.0, 1.0);',
+    //   '  gl_FragColor = vec4(color.xyz, alpha * opacity);',
+    //   alphaTest === 0
+    //     ? ''
+    //     : '  if (gl_FragColor.a < ' + alphaTest + ') discard;',
+    //   '}'
+    // ].join('\n')
   }, opt);
 };
